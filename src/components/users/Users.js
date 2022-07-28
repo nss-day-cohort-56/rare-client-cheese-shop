0f06ee7e41daee4af44ed7ea69b05a052f287610
Wesley Harllee <wesharllee@gmail.com>
2022-07-28 14:56:02 -0500

    User tab and pages w/ basic css

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,7 @@
 import { getAllUsers } from "./UserManager"
 import { useState } from "react"
 import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import "./Users.css"
 
 export const Users = () => {
     // const navigate = useNavigate()
@@ -11,36 +11,41 @@ export const Users = () => {
         getAllUsers().then(usersData => setUsers(usersData))
     }, [])
 
-    // //function to determine if user is staff
-    // const staff = (selectedUser) => {
-    //     if (selectedUser.is_staff = 1)
-    //     return "Yes"
-    //     else if (selectedUser.isStaff = 0)
-    //     return "No"
+    //function to determine if user is staff
+    const staff = (selectedUser) => {
+        if (selectedUser.is_staff === 1) {
+            return "Staff"
+        }
+        else {
+            return "Customer"
+        }
+    }
 
-    // }
     return (
         <>
-        <h2>User Title Test</h2>
-        <div className="post_container">
-            <h2>container test</h2>
-            {users.map((user) => {
-                let userName = user.username  
-                let firstName = user.first_name
-                let lastName = user.last_name
-                let email = user.email
-                let bio = user.bio
-                let password = user.password
-                let profileImg = user.profile_image_url
+            
+            <div className="user_container">
+                <div className="userTitle">Users</div>
+                {users.map((user) => {
+                    let userName = user.username
+                    let firstName = user.first_name
+                    let lastName = user.last_name
+                    let email = user.email
+                    let bio = user.bio
+                    let joinDate = user.created_on
+                    let profileImg = user.profile_image_url
+                    let profileType = staff(user)
 
                     return <section className="userBox" key={user.id}>
-                        <h3>All users Test</h3>
-                        <img src={profileImg} alt="userImage" className="userImage" />
-                        <div value={user.id}>User: {userName}</div>
+                        <div className="user" >
+
                         <div value={user.id}>Name: {firstName} {lastName}</div>
+                        <img src={profileImg} alt="userImage" className="userImage" />
+                        <div value={user.id}>Display Name: {userName}</div>
                         <div value={user.id}>Email: {email}</div>
-                        <div value={user.id}>bio: {bio}</div>
-                        <div value={user.id}>password: {password}</div>
+                        <div value={user.id}>Profile Type: {profileType}</div>
+                        <div value={user.id}>Date Joined: {Date(joinDate)}</div>
+                        </div>
                     </section>
                 })}
 
