9b8c8b76fc12f35544c7a45995c79b2ee2218971
Clinton Wilson <wilson.clinton.c@gmail.com>
2022-07-29 11:39:05 -0500

    adding tags to post in progress

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,27 +1,95 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
-import { getPostById } from "./PostManager"
+import { getAllPostTags, getAllTags } from "../tags/TagManager"
+import { addPost, getPostById } from "./PostManager"
 //allows user to see details of an individual post and navigate back to posts
 export const PostDetail = () => {
     const [post, setPost] = useState([])
+    const [postTags, setPostTags] = useState([])
+    const [tags, setTags] = useState([])
+    const [chosenTags, setChosenTags] = useState([])
+    const [isShown, setIsShown] = useState(false)
     const { postId } = useParams()
     const navigate = useNavigate()
+    const rareUser = localStorage.getItem("auth_token")
+    const rareUserObject = JSON.parse(rareUser)
 
     useEffect(() => {
         getPostById(postId)
-        .then(setPost)
+            .then(setPost)
     }, [postId])
 
-    
+    useEffect(() => {
+        getAllTags()
+            .then(setTags)
+    }, [])
 
-    return(
+    useEffect(() => {
+        getAllPostTags()
+            .then(setTags)
+    }, [])
+
+    const showTags = event => {
+        setIsShown(true)
+    }
+
+    const handleSaveTag = (event) => {
+        event.preventDefault()
+
+        addPost(post, chosenTags)
+        .then(getPostById(postId))
+        .then(setIsShown(false))
+    }
+
+    return (
         <section className="post">
             <h3 className="post__name">Title: {post.title}</h3>
-            <div className="post__image"><img src={post.image_url} alt ="postImage"/></div>
+            <div className="post__image"><img src={post.image_url} alt="postImage" /></div>
             <div className="post__content">Content: {post.content}</div>
             <div className="post__pub__date">Publication Date: {post.publication_date}</div>
             <div className="post__author__name">Author's Username: {post?.user?.username}</div>
-
+            {
+                postTags.map((pt) => {
+                    postId === pt.post_id
+                    ? <>
+                    
+                    </>
+                    : ""
+                })
+            }
+            {
+                rareUserObject === post.user_id
+                    ? <><button className="button" onClick={showTags}>Manage Tabs</button>
+                        {isShown && (<div id="checkedTags">
+                            {
+                                tags.map((tag) => {
+                                    return <>
+                                        <input key={`tag--${tag.id}`}
+                                            onChange={
+                                                (evt) => {
+                                                    const copy = new Set(chosenTags)
+                                                    const id = evt.target.id
+                                                    if (evt.target.checked) {
+                                                        copy.add(parseInt(id))
+                                                    } else {
+                                                        copy.delete(parseInt(id))
+                                                    }
+                                                    setChosenTags(copy)
+                                                }
+                                            }
+                                            type="checkbox"
+                                            name="tag"
+                                            id={tag.id}
+                                        />
+                                        {tag.label}
+                                    </>
+                                })
+                            }
+                            <button className="button" onClick={(clickEvent) => handleSaveTag(clickEvent)}>Save Tags To Post</button>
+                        </div>)}
+                    </>
+                    : ""
+            }
             <button className="button" onClick={() => {
                 navigate(`/posts`)
             }}>Back To Posts</button>
--- src/components/posts/PostManager.js
+++ src/components/posts/PostManager.js
@@ -20,4 +20,29 @@ export const getPostById = (id) => {
     .then(res => res.json())
 };
 
+export const addPost = (postObject, tagNumberSet) => {
+    return fetch(`http://localhost:8088/posts`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(postObject)
 
+    })
+        .then(response => response.json())
+        .then(newPost => {
+            const tagNumberArray = Array.from(tagNumberSet)
+
+            for (const tagNumber of tagNumberArray) {
+                fetch(`http://localhost:8088/postTags`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        post_id: newPost.id,
+                        tag_id: tagNumber
+                    })
+
+                })
+                .then(response => response.json())
+            }
+            
+        })
+}
